789dc17bef38cd88825b18c781009629a6f113f0
Jo Kerkhofs <jke@thinkerit.be>
2016-02-03 11:49:55 +0100

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,165 @@
+(function () {
+    'use strict';
+    angular.module('bin.theme', ['config', 'toggle.edit.mode'])
+        .service('binTheme', ['$q', 'configReader', 'configWriter', binThemeService])
+        .controller('colorPickerController', ['$rootScope', 'editModeRenderer', 'binTheme', colorPickerController])
+        .run(['$rootScope', 'binTheme', function ($rootScope, theme) {
+            if (!$rootScope.theme) $rootScope.theme = {};
+            if (!$rootScope.theme.color) $rootScope.theme.color = {};
+            theme.getPrimaryColor().then(function (color) {
+                $rootScope.theme.color.primary = color;
+            });
+        }]);
+
+    function binThemeService($q, reader, writer) {
+        var self = this;
+        this.defaultColor = '#000';
+        this.predefinedColors = ['#6abd45','#40b040','#338d7b','#dcda50','#dcc450','#dca850','#dc9150','#dc6950','#b94379','#7f3891','#554398','#3d6091'];
+
+        this.getPrimaryColor = function () {
+            var deferred = $q.defer();
+
+            reader({
+                $scope: {},
+                scope: 'public',
+                key: 'theme.primary.color'
+            }).then(function (result) {
+                var c = self.defaultColor;
+                if (isValidHexColor(result.data.value)) c = result.data.value;
+                else if (isLegacyThemeOption(result.data.value)) {
+                    var count = result.data.value.replace('theme-option-', '') - 1;
+                    c = count <= self.predefinedColors.length ? self.predefinedColors[count] : self.defaultColor;
+                }
+
+                deferred.resolve(c);
+            }, function () {
+                deferred.resolve(self.defaultColor);
+            });
+
+            return deferred.promise;
+        };
+
+        this.updatePrimaryColor = function (color) {
+            var deferred = $q.defer();
+
+            if (!isValidHexColor(color)) {
+                deferred.reject();
+            } else {
+                writer({
+                    $scope: {},
+                    scope: 'public',
+                    key: 'theme.primary.color',
+                    value: color
+                }).then(function () {
+                    deferred.resolve();
+                }, function () {
+                    deferred.reject();
+                });
+            }
+
+            return deferred.promise;
+        };
+
+        this.shadeColor = function (color, percent) {
+            if (color.length == 4) color = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
+            percent = percent / 100;
+            var f = parseInt(color.slice(1), 16),
+                t = percent < 0 ? 0 : 255,
+                p = percent < 0 ? percent * -1 : percent,
+                R = f >> 16,
+                G = f >> 8 & 0x00FF,
+                B = f & 0x0000FF,
+                RR = 0x1000000 + (Math.round((t - R) * p) + R) * 0x10000,
+                GG = (Math.round((t - G) * p) + G) * 0x100,
+                BB = (Math.round((t - B) * p) + B);
+            return "#" + (RR + GG + BB).toString(16).slice(1);
+        };
+
+        function isValidHexColor(color) {
+            return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
+        }
+
+        function isLegacyThemeOption(color) {
+            return /theme-option-[0-9]+/.test(color);
+        }
+    }
+
+    function colorPickerController($rootScope, editModeRenderer, theme) {
+        this.open = function () {
+            var rendererScope = $rootScope.$new();
+
+            editModeRenderer.open({
+                template: '<form ng-submit="save()">' +
+                '<div class="bin-menu-edit-body">' +
+
+                '<div class="alert alert-danger" ng-show="rejected">' +
+                '<i class="fa fa-exclamation-triangle"></i> ' +
+                '<span i18n code="theme.color.picker.invalid.color" read-only ng-bind="::var"></span>' +
+                '</div>' +
+
+                '<h4 i18n code="theme.color.picker.select.title" read-only ng-bind="::var"></h4>' +
+                '<div class="form-group">' +
+                '<button type="button" class="color-picker-block" ng-class="{active: selectedColor == c}" ' +
+                'ng-repeat="c in predefinedColors" ng-style="{backgroundColor: c, borderColor: c}" ' +
+                'ng-click="selectColor(c)">' +
+                '</button>' +
+                '</div>' +
+
+                '<hr>' +
+
+                '<div class="form-group">' +
+                '<table>' +
+                '<tr>' +
+                '<th style="padding-right: 15px;" i18n code="theme.color.picker.selected.color" read-only ng-bind="::var"></th>' +
+                '<td>' +
+                '<div class="input-group">' +
+                '<span class="input-group-addon" ng-style="{backgroundColor: selectedColor}">' +
+                '<span ng-hide="working"><i class="fa fa-fw"></i></span>' +
+                '<span ng-show="working"><i class="fa fa-spinner fa-fw fa-spin"></i></span>' +
+                '</span>' +
+                '<input type="text" class="form-control" style="width: 100px;" ng-model="selectedColor">' +
+                '</div>' +
+                '</td>' +
+                '</tr>' +
+                '</table>' +
+                '</div>' +
+                '</div>' +
+                '<div class="bin-menu-edit-actions">' +
+                '<button type="submit" class="btn btn-primary" i18n code="clerk.menu.save.button" read-only ng-bind="::var"></button>' +
+                '<button type="button" class="btn btn-default" ng-click="close()" i18n code="clerk.menu.close.button" read-only ng-bind="::var"></button>' +
+                '</div>' +
+                '</form>',
+                scope: rendererScope
+            });
+
+            rendererScope.working = true;
+            rendererScope.predefinedColors = theme.predefinedColors;
+
+            theme.getPrimaryColor().then(function (color) {
+                rendererScope.selectedColor = color;
+                rendererScope.working = false;
+            });
+
+            rendererScope.close = function () {
+                editModeRenderer.close();
+            };
+
+            rendererScope.selectColor = function (color) {
+                rendererScope.selectedColor = color;
+            };
+
+            rendererScope.save = function () {
+                rendererScope.working = true;
+                rendererScope.rejected = false;
+
+                theme.updatePrimaryColor(rendererScope.selectedColor).then(function () {
+                    $rootScope.theme.color.primary = rendererScope.selectedColor;
+                    editModeRenderer.close();
+                }, function () {
+                    rendererScope.rejected = true;
+                    rendererScope.working = false;
+                });
+            };
+        };
+    }
+})();
---
+++ test/theme.spec.js
@@ -0,0 +1,323 @@
+describe('bin.theme', function () {
+    var $rootScope, configReader, configWriter, configReaderDeferred, configWriterDeferred, editModeRenderer, theme;
+
+    var predefinedColors = ['#6abd45','#40b040','#338d7b','#dcda50','#dcc450','#dca850','#dc9150','#dc6950','#b94379','#7f3891','#554398','#3d6091'];
+
+    angular.module('config', [])
+        .value('config', {})
+        .factory('configReader', ['$q', function ($q) {
+            configReaderDeferred = $q.defer();
+            return jasmine.createSpy('configReader').and.returnValue(configReaderDeferred.promise);
+        }])
+        .factory('configWriter', ['$q', function ($q) {
+            configWriterDeferred = $q.defer();
+            return jasmine.createSpy('configWriter').and.returnValue(configWriterDeferred.promise);
+        }]);
+
+    angular.module('toggle.edit.mode', [])
+        .service('editModeRenderer', function () {
+            return jasmine.createSpyObj('editModeRenderer', ['open', 'close']);
+        });
+
+    beforeEach(module('bin.theme'));
+
+    beforeEach(inject(function (_$rootScope_, _configReader_, _configWriter_, _editModeRenderer_, binTheme) {
+        $rootScope = _$rootScope_;
+        configReader = _configReader_;
+        configWriter = _configWriter_;
+        editModeRenderer = _editModeRenderer_;
+        theme = binTheme;
+    }));
+
+    describe('on run', function () {
+        it('put primary color on rootScope', function () {
+            configReaderDeferred.resolve({data: {value: '#ccc'}});
+
+            $rootScope.$digest();
+
+            expect($rootScope.theme.color.primary).toEqual('#ccc');
+        });
+    });
+
+    describe('theme service', function () {
+        describe('on get primary color', function () {
+            var color;
+            beforeEach(function () {
+                color = undefined;
+
+                theme.getPrimaryColor().then(function (result) {
+                    color = result;
+                });
+            });
+
+            it('read primary theme color from config', function () {
+                expect(configReader).toHaveBeenCalledWith({
+                    $scope: {},
+                    scope: 'public',
+                    key: 'theme.primary.color'
+                });
+            });
+
+            it('when no config value, use default color', function () {
+                configReaderDeferred.reject();
+
+                $rootScope.$digest();
+
+                expect(color).toEqual('#000');
+            });
+
+            [
+                {actual: 'theme-option-1', expected: predefinedColors[0]},
+                {actual: 'theme-option-2', expected: predefinedColors[1]},
+                {actual: 'theme-option-3', expected: predefinedColors[2]},
+                {actual: 'theme-option-4', expected: predefinedColors[3]},
+                {actual: 'theme-option-5', expected: predefinedColors[4]},
+                {actual: 'theme-option-6', expected: predefinedColors[5]},
+                {actual: 'theme-option-7', expected: predefinedColors[6]},
+                {actual: 'theme-option-8', expected: predefinedColors[7]},
+                {actual: 'theme-option-9', expected: predefinedColors[8]},
+                {actual: 'theme-option-10', expected: predefinedColors[9]},
+                {actual: 'theme-option-11', expected: predefinedColors[10]},
+                {actual: 'theme-option-12', expected: predefinedColors[11]},
+                {actual: 'theme-option-13', expected: predefinedColors[12]},
+                {actual: 'theme-option-14', expected: '#000'},
+                {actual: '#ffffff', expected: '#ffffff'},
+                {actual: '#dcda50', expected: '#dcda50'},
+                {actual: '#000', expected: '#000'},
+                {actual: 'invalid', expected: '#000'}
+            ].forEach(function (test) {
+                it('when config value is ' + test.actual + ', expect ' + test.expected, function () {
+                    configReaderDeferred.resolve({data: {value: test.actual}});
+
+                    $rootScope.$digest();
+
+                    expect(color).toEqual(test.expected);
+                });
+            });
+        });
+
+        describe('on update primary color', function () {
+            var resolved, rejected;
+
+            describe('and color is valid', function () {
+                var newColor = '#aabbcc';
+
+                beforeEach(function () {
+                    resolved = undefined;
+                    rejected = undefined;
+
+                    theme.updatePrimaryColor(newColor).then(function () {
+                        resolved = true;
+                    }, function () {
+                        rejected = true;
+                    });
+                });
+
+                it('config writer is called', function () {
+                    expect(configWriter).toHaveBeenCalledWith({
+                        $scope: {},
+                        scope: 'public',
+                        key: 'theme.primary.color',
+                        value: newColor
+                    });
+                });
+
+                describe('on success', function () {
+                    beforeEach(function () {
+                        configWriterDeferred.resolve();
+
+                        $rootScope.$digest();
+                    });
+
+                    it('promise is resolved', function () {
+                        expect(resolved).toBeTruthy();
+                    });
+                });
+
+                describe('on reject', function () {
+                    beforeEach(function () {
+                        configWriterDeferred.reject();
+
+                        $rootScope.$digest();
+                    });
+
+                    it('promise is rejected', function () {
+                        expect(rejected).toBeTruthy();
+                    });
+                });
+            });
+
+            describe('and color is invalid', function () {
+                var invalidColor = 'invalid';
+
+                beforeEach(function () {
+                    resolved = undefined;
+                    rejected = undefined;
+
+                    theme.updatePrimaryColor(invalidColor).then(function () {
+                        resolved = true;
+                    }, function () {
+                        rejected = true;
+                    });
+
+                    $rootScope.$digest();
+                });
+
+                it('promise is rejected', function () {
+                    expect(rejected).toBeTruthy();
+                });
+            });
+        });
+
+        describe('on shadeColor', function () {
+            [
+                {actual: '#abc', percentage: 0, expected: '#aabbcc'},
+                {actual: '#3f83a3', percentage: 0, expected: '#3f83a3'},
+                {actual: '#3f83a3', percentage: 10, expected: '#528fac'},
+                {actual: '#3f83a3', percentage: 20, expected: '#659cb5'},
+                {actual: '#3f83a3', percentage: 30, expected: '#79a8bf'},
+                {actual: '#3f83a3', percentage: 40, expected: '#8cb5c8'},
+                {actual: '#3f83a3', percentage: 50, expected: '#9fc1d1'},
+                {actual: '#3f83a3', percentage: 60, expected: '#b2cdda'},
+                {actual: '#3f83a3', percentage: 70, expected: '#c5dae3'},
+                {actual: '#3f83a3', percentage: 80, expected: '#d9e6ed'},
+                {actual: '#3f83a3', percentage: 90, expected: '#ecf3f6'},
+                {actual: '#3f83a3', percentage: 100, expected: '#ffffff'},
+                {actual: '#3f83a3', percentage: -10, expected: '#397693'},
+                {actual: '#3f83a3', percentage: -20, expected: '#326982'},
+                {actual: '#3f83a3', percentage: -30, expected: '#2c5c72'},
+                {actual: '#3f83a3', percentage: -40, expected: '#264f62'},
+                {actual: '#3f83a3', percentage: -50, expected: '#204252'},
+                {actual: '#3f83a3', percentage: -60, expected: '#193441'},
+                {actual: '#3f83a3', percentage: -70, expected: '#132731'},
+                {actual: '#3f83a3', percentage: -80, expected: '#0d1a21'},
+                {actual: '#3f83a3', percentage: -90, expected: '#060d10'},
+                {actual: '#3f83a3', percentage: -100, expected: '#000000'}
+            ].forEach(function (test) {
+                it('shade color by ' + test.percentage * 100 + ' percent', function () {
+                    expect(theme.shadeColor(test.actual, test.percentage)).toEqual(test.expected);
+                });
+            });
+        });
+    });
+
+    describe('colorPicker controller', function () {
+        var ctrl;
+
+        beforeEach(inject(function ($controller) {
+            ctrl = $controller('colorPickerController');
+        }));
+
+        describe('on open', function () {
+            beforeEach(function () {
+                ctrl.open();
+
+                configReaderDeferred.resolve({data: {value: '#ccc'}});
+                $rootScope.$digest();
+            });
+
+            it('editMode renderer is opened', function () {
+                expect(editModeRenderer.open).toHaveBeenCalledWith({
+                    template: jasmine.any(String),
+                    scope: jasmine.any(Object)
+                });
+            });
+
+            describe('with renderer scope', function () {
+                var scope;
+
+                beforeEach(function () {
+                    scope = editModeRenderer.open.calls.mostRecent().args[0].scope;
+                });
+
+                it('predefined colors are available', function () {
+                    expect(scope.predefinedColors).toEqual(predefinedColors);
+                });
+
+                it('initialize selected color', function () {
+                    expect(scope.selectedColor).toEqual('#ccc');
+                });
+
+                it('when new color is selected', function () {
+                    scope.selectColor('#ffffff');
+
+                    expect(scope.selectedColor).toEqual('#ffffff');
+                });
+
+                describe('on save', function () {
+                    describe('with valid color', function () {
+                        var newColor = '#aabbcc';
+
+                        beforeEach(function () {
+                            scope.selectedColor = newColor;
+                            scope.save();
+                        });
+
+                        it('working is on scope', function () {
+                            expect(scope.working).toBeTruthy();
+                        });
+
+                        describe('on success', function () {
+                            beforeEach(function () {
+                                configWriterDeferred.resolve();
+
+                                $rootScope.$digest();
+                            });
+
+                            it('update rootScope with new value', function () {
+                                expect($rootScope.theme.color.primary).toEqual(newColor);
+                            });
+
+                            it('editModeRenderer is closed', function () {
+                                expect(editModeRenderer.close).toHaveBeenCalled();
+                            });
+                        });
+
+                        describe('on reject', function () {
+                            beforeEach(function () {
+                                configWriterDeferred.reject();
+
+                                $rootScope.$digest();
+                            });
+
+                            it('rejected is on scope', function () {
+                                expect(scope.rejected).toBeTruthy();
+                            });
+
+                            it('editModeRenderer is not closed', function () {
+                                expect(editModeRenderer.close).not.toHaveBeenCalled();
+                            });
+
+                            it('working is false', function () {
+                                expect(scope.working).toBeFalsy();
+                            });
+                        });
+                    });
+
+                    describe('with invalid color', function () {
+                        beforeEach(function () {
+                            scope.selectedColor = 'invalid';
+                            scope.save();
+
+                            $rootScope.$digest();
+                        });
+
+                        it('rejected is on scope', function () {
+                            expect(scope.rejected).toBeTruthy();
+                        });
+
+                        it('working is false', function () {
+                            expect(scope.working).toBeFalsy();
+                        });
+                    });
+                });
+
+                it('on close, editModeRenderer is closed', function () {
+                    scope.close();
+
+                    expect(editModeRenderer.close).toHaveBeenCalled();
+                });
+            });
+        });
+    });
+});
